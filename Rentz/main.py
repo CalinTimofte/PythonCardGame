2697f8c086c2122ddcdb8e40da879ddf001ac58f
Calin Timofte <calin.timofte@yahoo.com>
2020-08-26 21:17:50 +0300

    feat: Add Game functions
    Game class functions for showing all the cards in the deck (show), shuffling the cards in the deck (shuffle) and dealing the cards (spread cards).

--- Rentz/main.py
+++ Rentz/main.py
@@ -16,3 +16,22 @@ class Game:
     def __init__(self):
         self.cards = [Card(i, j) for i in ["Heart", "Spade", "Diamond", "Clubs"] for j in range(7, 15)]
         self.players = list()
+
+    def show(self):
+        for i in self.cards:
+            print(i.number, i.suit)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def spread_cards(self):
+        p1 = list()
+        p2 = list()
+        p3 = list()
+        p4 = list()
+        while self.cards:
+            p1.append(self.cards.pop())
+            p2.append(self.cards.pop())
+            p3.append(self.cards.pop())
+            p4.append(self.cards.pop())
+        self.players = [Player(i) for i in [p1, p2, p3, p4]]
